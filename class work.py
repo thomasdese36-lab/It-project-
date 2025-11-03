b17dba8925b43cd563a29820f189b113395a239d
thomasdese36-lab <thomasdese36@gmail.com>
2025-11-03 12:06:48 +0300

    Add files via upload

---
+++ class work.py
@@ -0,0 +1,15 @@
+
+
+x=int(input ("how old are u?"))
+if x<=12:
+    print ("child")
+elif x<= 17:
+    print ("teenager")   
+elif x<= 64:
+    print ("Adult")
+    
+else:
+    print("senior")
+   
+    
+    
